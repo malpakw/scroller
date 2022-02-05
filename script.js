c081f151c1860a20c08dd21d02b900bf37f9d374
malpakw <marcin.zietara.kw@gmail.com>
2022-02-05 17:10:29 +0100

    scrollIntoView

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('hello, world');
 
     const rootElement = document.querySelector('#root')
     const sections = document.querySelectorAll('section')
+    let currentSectionIndex = 0;
+    let isThrottled = false;
 
-    document.addEventListener('mousewheel', function(e) {
-        console.log(e.wheelDelta)
+    document.addEventListener('wheel', function(event) {
+        const direction = event.deltaY > 0 ? 1 : -1;
+        if (isThrottled) return
+        isThrottled = true
+
+        setTimeout(function() {
+            isThrottled = false;
+        }, 1000)
+
+        if (direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length - 1
+        if (isLastSection) return;
+        } else if (direction === -1) {
+            const FirstSection = currentSectionIndex === 0;
+            if (FirstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction
+        console.log(currentSectionIndex)
+
+        sections[currentSectionIndex].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
     })
 })
